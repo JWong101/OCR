b18ebad1ccad7df74e4425b3ab676fe983b7b0bf
JWong101 <joshua99wong@gmail.com>
2018-02-23 23:56:23 -0600

    Preprocess Data

--- vision.py
+++ vision.py
@@ -1,6 +1,24 @@
 import cv2
 import numpy as np
 
+size = 20
+NUM_DIGITS = 10
+
+def split2d(img, cell_size):
+    height, width = img.shape[:2]
+    sx, sy = cell_size
+    cells = [np.hsplit(row, width / sx) for row in np.vsplit(img, height / sy)]
+    cells = np.array(cells) 
+    #flatten the data
+    cells = cells.reshape(-1, sx, sy)
+    return cells
+
+def getLabelsAndTraining(img):
+    digits = split2d(img, (size, size))
+    #gets labels for digits repeats 0-10 number of times each appear in picture
+    labels = np.repeat(np.arange(NUM_DIGITS), len(digits) / NUM_DIGITS)
+    return digits, labels
+
 def deskew(img):
     moments = cv2.moments(img)
 
@@ -9,12 +27,11 @@ def deskew(img):
 
     skew = moments['mu11'] / moments['mu02']
 
-    size = img.size()
-    trans = np.float32([[1, skew, ]-0.5 * size * skew, [0, 1, 0])
+    trans = np.float32([[1, skew, -0.5 * size * skew], [0, 1, 0]])
     img = cv2.warpAffine(img, trans, (size, size), flags= cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
     return img
 
-def singleHog(img):
+def singleHog():
     winSize = (20, 20) #size of image 20x20
     blockSize = (10, 10) #used for illumination variation. Typically it's 2 * cellSize, however illumination doesn't matter in this case
     blockStride = (5, 5) #overlap between neighbloring blocks. 50% of blockSize
@@ -28,9 +45,8 @@ def singleHog(img):
     gammaCorrection = 1
     nlevels = 64
     useSignedGradients = True #signed gradients are between 0 and 360, unsigned 0 and 180 (pointing up or down)
-    hog = cv2.HogDescriptor(winSize, blockSize, blockStride, cellSize, nBins, derivAperture, winSigma, histogramNormType, L2HysThreshold, gammaCorrection, nlevels, useSignedGradients)
-
-    descript = hog.compute(img)
+    hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nBins, derivAperture, winSigma, histogramNormType, L2HysThreshold, gammaCorrection, nlevels, useSignedGradients)
+    return hog
 
 def train():
     svm = cv2.ml.SVM_create()
@@ -52,4 +68,26 @@ def train():
     #autotune parameters
     #takes 5x longer to train
 #    svm.trainAuto(trainData)
+
+if __name__ == '__main__':
+    
+    #extract each 20x20 digit and label
+    img = cv2.imread("digits.png", 0)
+    digits, labels = getLabelsAndTraining(img)
+    #shuffle data
+    rand = np.random.RandomState(10)
+    shuffle = rand.permutation(len(digits))
+    digits, labels = digits[shuffle], labels[shuffle]
+
+    deskewed_digits = list(map(deskew, digits))
+    
+    hog = singleHog()
+    descriptors = list(map(hog.compute, deskewed_digits))
+    
+    descriptors = np.squeeze(descriptors)
+
+    numTrain = int(0.9 * len(descriptors))
+    digits_train, digits_test = np.split(deskewed_digits, [numTrain])
+    descriptors_train, descriptors_test = np.split(descriptors, [numTrain])
+    labels_train, labels_test = np.split(labels, [numTrain])
 
